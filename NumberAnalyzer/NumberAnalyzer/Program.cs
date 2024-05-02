f1d518334d303abc74f6718fe0620c98276622a0
Tae Wuh <runbho@gmail.com>
2024-05-02 10:47:07 -0400

    Lab1

---
+++ NumberAnalyzer/NumberAnalyzer/Program.cs
@@ -0,0 +1,34 @@
+﻿string userInput;
+string userName;
+int num;
+
+Console.WriteLine("What is your name?");
+userName = Console.ReadLine();
+Console.WriteLine($"{userName}, please enter an interger between 1 and 100 inclusive.");
+userInput = Console.ReadLine();
+num = int.Parse(userInput);
+
+if (num % 2 != 0 && num < 60)
+{
+    Console.WriteLine($"{userName}, you have entered {num}, odd and less than 60.");
+}
+else if (num % 2 == 0 && num >=2 && num <= 24)
+{
+    Console.WriteLine($"{userName}, you have entered {num}, even and less than 25.");
+}
+else if (num % 2 == 0 && num >=26 && num <= 60)
+{
+    Console.WriteLine($"{userName}, you have entered {num}, even and between 26 and 60 inclusive.");
+}
+else if (num % 2 == 0 && num > 60 && num<= 100)
+{
+    Console.WriteLine($"{userName}, you have entered {num}, even and greater than 60.");
+}
+else if (num % 2 != 0 && num < 60 && num <= 100)
+{
+    Console.WriteLine($"{userName}, you have entered {num}, odd and greater than 60.");
+}
+else
+{
+    Console.WriteLine($"{userName}, you need to enter an integer between 1 and 100 inclusive.");
+}
